f0930b7d66e32eb2463d1dbb978d25dfd88f9c44
fraeron <rafaeljtenido@gmail.com>
2024-04-19 14:27:20 +0800

    Add main files

---
+++ src/carems/App.java
@@ -0,0 +1,9 @@
+package carems;
+
+import carems.gui.MainMenu;
+
+public class App {
+    public static void main(String[] args) {
+        MainMenu mainMenu = new MainMenu();
+    }
+}
---
+++ src/carems/gui/MainMenu.java
@@ -0,0 +1,13 @@
+package carems.gui;
+
+import javax.swing.JFrame;
+
+public class MainMenu extends JFrame {
+    public MainMenu(){  
+        setTitle("Carems");
+        setSize(800,600);  
+        setLayout(null);  
+        setLocationRelativeTo(null);
+        setVisible(true);  
+    }
+}
