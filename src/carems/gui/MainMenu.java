3c7bf0dc3bc92a94626abbc2c9fc2dbac5e42859
fraeron <163626051+fraeron@users.noreply.github.com>
2024-05-13 14:36:34 +0800

    Update MainMenu.java
    Combined MainMenu JPanel code.
    Changed comments.
    Explicitly typed import statements.
    Added final to widget variables.
    Removed unused lines.

--- src/carems/gui/MainMenu.java
+++ src/carems/gui/MainMenu.java
@@ -1,11 +1,60 @@
 package carems.gui;
 
-import javax.swing.JFrame;
 
-public class MainMenu extends JFrame {
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+
+
+class MainMenuPanel extends JPanel {
+    private final Dimension panelSize = new Dimension(800,600);
+    
+    private final Color clrAshGrey = new Color(42, 42, 42);    
+    private final Color clrMagmaOrange = new Color(255, 127, 39);
+    
+    MainMenuPanel(){
+        this.setPreferredSize(panelSize);
+        this.setBackground(clrAshGrey);   
+    }
+    
+    // Paint lines in the panel.
+    @Override
+    public void paint(Graphics g) { 
+        
+        Graphics2D line1 = (Graphics2D) g;
+        Graphics2D line2 = (Graphics2D) g;
+
+        line1.setColor(clrMagmaOrange);
+        line2.setColor(clrMagmaOrange);
+        
+        line1.drawLine(200, 200, 0, 200);
+        line2.drawLine(200, 0, 200, 800);
+    }
+}
+
+public class MainMenu extends JFrame implements ActionListener {
+    
+    private final MainMenuPanel panel;
+    private final JLabel lblCarems;
+    private final JButton btnBookingDetail, btnCar, btnOwner, btnCustomer, btnLogout;
+    
+    // Add color.
+    private final Color clrAshGrey = new Color(42, 42, 42);    
+    private final Color clrMagmaOrange = new Color(255, 127, 39);
+    
     public MainMenu(){  
-      
-        panel = new MainMenuJPanel();
+
+        panel = new MainMenuPanel();
         
         lblCarems = new JLabel();
         lblCarems.setText("CAREMS");
@@ -43,11 +92,10 @@ public class MainMenu extends JFrame {
         btnLogout.setFont(new Font("League Spartan", Font.BOLD, 12));
         btnLogout.addActionListener(this);
         
-        //JFRAME for Main Menu
+        // Add MainMenu properties.
         this.setTitle("Carems - Car Rental Management System");
         this.setSize(800,600);
         this.setResizable(false);
-//        this.setLayout(null);
         this.setVisible(true);
         this.setLocationRelativeTo(null);
         
@@ -57,19 +105,18 @@ public class MainMenu extends JFrame {
         this.add(btnCar);
         this.add(btnOwner);
         this.add(btnLogout);
-        this.add(panel); //the panel and lines from MainMenuJPanel
+        this.add(panel);
         this.pack();
         
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
         
-        this.setBackground(clrAshGrey); //BG Color
+        this.setBackground(clrAshGrey);
            
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         if(e.getSource() == btnCustomer){
-            
         }
         else if(e.getSource() == btnBookingDetail){
         
@@ -79,18 +126,8 @@ public class MainMenu extends JFrame {
             this.dispose();
         }
         else if(e.getSource() == btnOwner){
-        
         }
         else if(e.getSource() == btnLogout){
-        
         }
-
-        setTitle("Carems");
-        setSize(800,600);  
-        setLayout(null);  
-        setLocationRelativeTo(null);
-        setVisible(true);  
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-
     }
 }
