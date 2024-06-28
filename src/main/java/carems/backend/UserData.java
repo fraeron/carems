721d020b2bcf88fb088e68ba0f03cbd994596963
GeloTenids <matenido@gmail.com>
2024-06-28 20:59:04 +0800

    Connected login to database

--- src/main/java/carems/backend/UserData.java
+++ src/main/java/carems/backend/UserData.java
@@ -1,22 +1,40 @@
 package carems.backend;
 
+import java.sql.*;
+
 public class UserData {
 
-    private final String[][] sampleData = {
-        {"walterWhite", "Heisenberg"},
-        {"unSya192", "sleezyRabbit"},
-        {"RonATT", "ohRon"},
-        {"OptimumPride", "autoBots"},
-        {"Carems", "OOP"}
-    };
+//    private final String[][] sampleData = {
+//        {"walterWhite", "Heisenberg"},
+//        {"unSya192", "sleezyRabbit"},
+//        {"RonATT", "ohRon"},
+//        {"OptimumPride", "autoBots"},
+//        {"Carems", "OOP"}
+//    };
 
     public Boolean getResult(String userName, String password) {
-        for (String[] user : sampleData) {
-            if (user[0].equalsIgnoreCase(userName) && user[1].equals(password)) {
-                return true;
+        boolean result = false;
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/db_carems", "root", "password123!");
+            String query = "SELECT userName, password FROM tbl_user WHERE username=? and password=?";
+            PreparedStatement pst = con.prepareStatement(query);
+            pst.setString(1, userName);
+            pst.setString(2, password);
+            
+            ResultSet rs = pst.executeQuery();
+            if (rs.next()) {
+                result = true;
+            } else {
+                result = false;
             }
-        }
-        return false;
+            rs.close();
+            pst.close();
+            con.close();
+            }catch(ClassNotFoundException | SQLException ex){
+                ex.printStackTrace();
+            }
+        return result;
     }
 
 }
